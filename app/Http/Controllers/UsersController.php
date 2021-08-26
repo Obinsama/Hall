662ec6f461a0091b9bcdcb358e83948307c7dfc7
Obinsama <aubinsama@gmail.com>
2021-08-26 10:29:44 +0100

    update workers page 002

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -27,7 +27,7 @@ class UsersController extends Controller
             $data='Bonjour';
 //            return response()->json(request('search'));
         }else{
-            $data = User::orderBy('id','DESC')->get();
+            $data = User::orderBy('id','DESC')->paginate(35);
             return response()->json($data);
         }
         return response()->json(request('search'));
